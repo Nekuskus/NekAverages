b87a7b0b492eee0a7abd7d8b7c1d6a9958871f7e
Nekuskus <61391965+Nekuskus@users.noreply.github.com>
2020-05-26 15:31:01 +0200

    Added base for the project

--- Class1.cs
+++ Class1.cs
@@ -1,8 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
 
 namespace NekAverages
 {
-    public class Class1
+    /// <summary>
+    /// This class is for making objects which will be added to the dictionary
+    /// </summary>
+    public class Grade
     {
+        private int _weight;
+        private float _value;
+        public int Weight
+        {
+            get => _weight;
+            set => _weight = value;
+        }
+        public float Value
+        {
+            get => _value;
+            set => _value = value;
+        }
+    }
+    public class Averages
+    {
+        /// <summary>
+        /// This method creates base files for around 20 base supported subjects, as the files will be overwritten the client should include a warning before performing this action. Just for the safety if they exists they will be copied to the \Backups folder.
+        /// </summary>
+        public void Init()
+        {
+
+        }
+        public Dictionary<string,int> Final;
+        public Dictionary<string,Grade[]> Subjects;
+        public Dictionary<string,Grade[]> MidYearSubjects;
+        public void Main()
+        {
+            
+        }
     }
 }
